fde473dc64abab174d8471d564dae7def4ceeef9
Vasyl Martyniuk <v.martyn.ua@gmail.com>
2020-11-10 21:26:55 +0200

    [~] all files

--- test-project/script.js
+++ test-project/script.js
@@ -7,5 +7,11 @@ $('.multiple-items').slick({
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 3,
+    // vertical: true,
     nextArrow: "<img src='images/arr-right.svg' class='next' alt='2'>",
 });
+
+var x = document.querySelector('#xxxx1')
+x.style.width = '88%';
+var y = document.querySelector('#yyyy1')
+y.innerHTML = x.style.width;
